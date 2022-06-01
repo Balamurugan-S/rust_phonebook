451ca30fe3340d19dadf6a46250be5a4bed369d5
Balamurugan-S <sbalamurugan1496@gmail.com>
2022-06-01 17:00:51 +0200

    Try to maximize Rust features move non-main related code to new file create functions for repeated code consider adding some error messages

---
+++ src/lib.rs
@@ -0,0 +1,129 @@
+use std::io;
+
+pub struct PhoneBook {
+    pub name: Vec<String>,
+    pub number: Vec<String>,
+}
+
+impl PhoneBook {
+    pub fn list(&self) {
+        println!("NAME \t\t CONTACT NUMBER");
+        println!("------------------------");
+        for i in 0..self.name.len() {
+            println!("{} \t\t {}", self.name[i], self.number[i]);
+        }
+    }
+
+    pub fn add(&mut self) {
+        self.name.push(Self::get_user_name(&self));
+
+        self.number.push(Self::get_user_number(&self));
+    }
+
+    pub fn delete(&mut self) {
+        let mut name_to_be_deleted = String::new();
+        println!("Enter Name to be deleted: ");
+        io::stdin()
+            .read_line(&mut name_to_be_deleted)
+            .expect("No username");
+        let name_to_be_deleted: String = name_to_be_deleted
+            .trim()
+            .parse()
+            .expect("Name entered was not a valid");
+
+        if !Self::check_empty(&self) {
+            for name_idx in 0..self.name.len() {
+                if self.name[name_idx] == (name_to_be_deleted) {
+                    println!("DELETED {}", self.name[name_idx]);
+                    self.name.remove(name_idx);
+                    self.number.remove(name_idx);
+                } else {
+                    println!("Not found");
+                }
+            }
+        }
+    }
+
+    pub fn find(&mut self) {
+        let mut name_to_be_found = String::new();
+        println!("Enter Name to be found: ");
+        io::stdin()
+            .read_line(&mut name_to_be_found)
+            .expect("No username");
+        let name_to_be_found: String = name_to_be_found
+            .trim()
+            .parse()
+            .expect("Name entered was not a valid");
+
+        if !Self::check_empty(&self) {
+            for name_idx in 0..self.name.len() {
+                if self.name[name_idx] == (name_to_be_found) {
+                    println!("Found {}", self.name[name_idx]);
+                    println!("\t Name:   {}", self.name[name_idx]);
+                    println!("\t Number: {}", self.number[name_idx]);
+                } else {
+                    println!("Not Found {}", name_to_be_found);
+                }
+            }
+        }
+    }
+
+    fn get_user_name(&self) -> String {
+        let mut user_name = String::new();
+        println!("Enter Name: ");
+        io::stdin().read_line(&mut user_name).expect("No username");
+        let user_name: String = user_name
+            .trim()
+            .parse()
+            .expect("Name entered was not a valid");
+        user_name
+    }
+
+    fn get_user_number(&self) -> String {
+        let mut user_number = String::new();
+        println!("Enter Number: ");
+        io::stdin()
+            .read_line(&mut user_number)
+            .expect("No usernumber");
+        let user_number: String = user_number
+            .trim()
+            .parse()
+            .expect("Number entered was not a valid");
+        user_number
+    }
+
+    fn check_empty(&self) -> bool {
+        if self.name.is_empty() {
+            println!("No entries yet, Consider adding some info");
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+pub fn menu() {
+    println!("*---------------------*");
+    println!("My RUSTY Phonebook");
+    println!("*---------------------*");
+    println!("1. List   Contacts");
+    println!("2. Add    Contacts");
+    println!("3. Delete Contacts");
+    println!("4. Find   Contacts");
+    println!("5. EXIT");
+    println!("*---------------------*");
+    println!("Enter your choice: ");
+}
+
+pub fn get_user_choice() -> u8 {
+    let mut user_choice = String::new();
+
+    io::stdin().read_line(&mut user_choice).expect("No input");
+
+    let user_choice: u8 = user_choice
+        .trim()
+        .parse()
+        .expect("user_choice entered was not a number");
+
+    user_choice
+}
--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,6 @@
-use std::io;
 use std::process;
 
-struct PhoneBook {
-    name: Vec<String>,
-    number: Vec<String>,
-}
-
-impl PhoneBook {
-    fn list(&self) {
-        println!("NAME \t\t CONTACT NUMBER");
-        println!("------------------------");
-        for i in 0..self.name.len() {
-            println!("{} \t\t {}", self.name[i], self.number[i]);
-        }
-    }
-
-    fn add(&mut self) {
-        let mut user_name = String::new();
-        println!("Enter Name: ");
-        io::stdin().read_line(&mut user_name).expect("No username");
-        let user_name: String = user_name
-            .trim()
-            .parse()
-            .expect("Name entered was not a valid");
-        self.name.push(user_name);
-
-        let mut user_number = String::new();
-        println!("Enter Number: ");
-        io::stdin()
-            .read_line(&mut user_number)
-            .expect("No usernumber");
-        let user_number: String = user_number
-            .trim()
-            .parse()
-            .expect("Number entered was not a valid");
-        self.number.push(user_number);
-    }
-
-    fn delete(&mut self) {
-        let mut name_to_be_deleted = String::new();
-        println!("Enter Name to be deleted: ");
-        io::stdin()
-            .read_line(&mut name_to_be_deleted)
-            .expect("No username");
-        let name_to_be_deleted: String = name_to_be_deleted
-            .trim()
-            .parse()
-            .expect("Name entered was not a valid");
-
-        for name_idx in 0..self.name.len() {
-            if self.name[name_idx] == (name_to_be_deleted) {
-                println!("found at index {}", name_idx);
-                self.name.remove(name_idx);
-                self.number.remove(name_idx);
-            }
-        }
-    }
-
-    fn find(&mut self) {
-        let mut name_to_be_found = String::new();
-        println!("Enter Name to be found: ");
-        io::stdin()
-            .read_line(&mut name_to_be_found)
-            .expect("No username");
-        let name_to_be_found: String = name_to_be_found
-            .trim()
-            .parse()
-            .expect("Name entered was not a valid");
-
-        for name_idx in 0..self.name.len() {
-            if self.name[name_idx] == (name_to_be_found) {
-                println!("Found {}", self.name[name_idx]);
-                println!("\t Name:   {}", self.name[name_idx]);
-                println!("\t Number: {}", self.number[name_idx]);
-            }
-        }
-    }
-}
+use my_phonebook::PhoneBook;
 
 fn main() {
     let mut phonebook_instance = PhoneBook {
@@ -85,16 +9,9 @@ fn main() {
     };
 
     loop {
-        menu();
+        my_phonebook::menu();
 
-        let mut user_choice = String::new();
-
-        io::stdin().read_line(&mut user_choice).expect("No input");
-
-        let user_choice: u8 = user_choice
-            .trim()
-            .parse()
-            .expect("user_choice entered was not a number");
+        let user_choice = my_phonebook::get_user_choice();
 
         match user_choice {
             1 => phonebook_instance.list(),
@@ -105,17 +22,4 @@ fn main() {
             _ => panic!("Wrong Choice"),
         }
     }
-}
-
-fn menu() {
-    println!("*---------------------*");
-    println!("My RUSTY Phonebook");
-    println!("*---------------------*");
-    println!("1. List   Contacts");
-    println!("2. Add    Contacts");
-    println!("3. Delete Contacts");
-    println!("4. Find   Contacts");
-    println!("5. EXIT");
-    println!("*---------------------*");
-    println!("Enter your choice: ");
 }
