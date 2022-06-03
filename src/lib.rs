1df277bf502fc4da34db4f347462fb0f8f7e5909
Balamurugan-S <sbalamurugan1496@gmail.com>
2022-06-03 14:59:03 +0200

    Add ErrorKind for Result type

--- src/lib.rs
+++ src/lib.rs
@@ -20,58 +20,61 @@ impl PhoneBook {
         match Self::get_user_name(&self) {
             Ok(user_name) => self.name.push(user_name),
             Err(error) => println!("{}", error),
-        }
+        };
 
         match Self::get_user_number(&self) {
             Ok(user_number) => self.number.push(user_number),
             Err(error) => println!("{}", error),
-        }
+        };
     }
 
     pub fn delete(&mut self) {
         match Self::get_user_name(&self) {
             Ok(name_to_be_deleted) => {
-                if !Self::check_empty(&self) {
-                    for name_idx in 0..self.name.len() {
-                        if self.name[name_idx] == (name_to_be_deleted) {
-                            println!("DELETED {}", self.name[name_idx]);
-                            self.name.remove(name_idx);
-                            self.number.remove(name_idx);
-                            break;
-                        }
+                for name_idx in 0..self.name.len() {
+                    if self.name[name_idx] == (name_to_be_deleted) {
+                        println!("DELETED {}", self.name[name_idx]);
+                        self.name.remove(name_idx);
+                        self.number.remove(name_idx);
+                        break;
                     }
                 }
             }
-            Err(error) => println!("{}", error),
-        }
+            Err(error) => {
+                println!("{}", error);
+            }
+        };
     }
 
     pub fn find(&mut self) {
         match Self::get_user_name(&self) {
             Ok(name_to_be_found) => {
-                if !Self::check_empty(&self) {
-                    for name_idx in 0..self.name.len() {
-                        if self.name[name_idx] == (name_to_be_found) {
-                            println!("Found {}", self.name[name_idx]);
-                            println!("\t Name:   {}", self.name[name_idx]);
-                            println!("\t Number: {}", self.number[name_idx]);
-                            break;
-                        }
+                for name_idx in 0..self.name.len() {
+                    if self.name[name_idx] == (name_to_be_found) {
+                        println!("Found {}", self.name[name_idx]);
+                        println!("\t Name:   {}", self.name[name_idx]);
+                        println!("\t Number: {}", self.number[name_idx]);
+                        break;
                     }
                 }
             }
-            Err(error) => println!("{}", error),
-        }
+            Err(error) => {
+                println!("{}", error);
+            }
+        };
     }
 
     fn get_user_name(&self) -> Result<String, Box<dyn Error>> {
         let mut user_name = String::new();
         println!("Enter Name: ");
-        io::stdin().read_line(&mut user_name).expect("No username");
-        let user_name: String = user_name
-            .trim()
-            .parse()
-            .expect("Name entered was not a valid");
+        io::stdin().read_line(&mut user_name)?;
+        let user_name = user_name.trim().to_string();
+        if user_name.is_empty() {
+            return Err(Box::new(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "Enter valid input",
+            )));
+        }
         Ok(user_name)
     }
 
@@ -79,11 +82,17 @@ impl PhoneBook {
         let mut user_number = String::new();
         println!("Enter Number: ");
         io::stdin().read_line(&mut user_number)?;
-        let user_number: String = user_number.trim().parse()?;
+        let user_number = user_number.trim().to_string();
+        if user_number.is_empty() {
+            return Err(Box::new(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "Enter valid input",
+            )));
+        }
         Ok(user_number)
     }
 
-    fn check_empty(&self) -> bool {
+    fn _check_empty(&self) -> bool {
         if self.name.is_empty() {
             println!("No entries yet, Consider adding some info");
             return true;
